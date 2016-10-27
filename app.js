15650a1d4f52c1b56d87df5d4d2d0bcdf17b664c
Sha <shashashasha@gmail.com>
2016-10-27 01:35:42 -0400

    Refactoring to use async.waterfall and adding route to add people

--- app.js
+++ app.js
@@ -1,81 +1,126 @@
-// Copyright 2015-2016, Google, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+
+  MR AND MRS HWANG ARE HAVING A PARTY
+
+*/
+
 
 // [START app]
 'use strict';
 
 var express = require('express');
 var SETTINGS = require('./settings.json');
+var creds = require('./google-generated-creds.json');
 var GoogleSpreadsheet = require('google-spreadsheet');
 var async = require('async');
 
 var app = express();
 
+// Index path just shows everyone
 app.get('/', function (req, res) {
 
-  // spreadsheet key is the long id in the sheets URL
-  var doc = new GoogleSpreadsheet(SETTINGS.SHEET);
-  var sheet;
-
-  async.series([
-    function setAuth(step) {
-      // see notes below for authentication instructions!
-      var creds = require('./google-generated-creds.json');
-
-      // OR, if you cannot save the file locally (like on heroku)
-      var creds_json = {
-        client_email: creds.client_email,
-        private_key: creds.private_key
-      }
-
-      doc.useServiceAccountAuth(creds, step);
-    },
-    function getInfoAndWorksheets(step) {
-      doc.getInfo(function(err, info) {
-        if (err) {
-          res.status(200).send(err);
-        }
-
-        console.log('Loaded doc: ' + info.title + ' by ' + info.author.email);
-
-        sheet = info.worksheets[0];
-
-        console.log('Sheet 1: ' + sheet.title + ' ' + sheet.rowCount + 'x' + sheet.colCount);
-        step();
-      });
-    },
-    function workingWithCells(step) {
-      sheet.getCells({
-        'min-row': 3,
-        'max-row': 101,
-        'min-col': 3,
-        'max-col': 3,
-        'return-empty': true
-      }, function(err, cells) {
-        var list = "";
-
-        cells.forEach(function(cell, i) {
-          list += cell.value + ', ';
-        });
-
-        res.status(200).send(list);
-
-        step();
-      });
+  // Thank you Javascript for this beautiful structure...
+  async.waterfall([
+    async.apply(getGoogleSheet),
+    async.apply(getPeople),
+    async.apply(showPeopleTable),
+    function (msg, callback) {
+      // Output list of people
+      res.status(200).send(msg);
     }
   ]);
 });
+
+// Simple path to add someone to the spreadsheet
+app.get('/add/:person/:number', function (req, res) {
+
+  async.waterfall([
+    async.apply(getGoogleSheet),
+
+    // Here's the good stuff
+    function (sheet, callback) {
+
+      // Create the new person
+      var person = {
+        name: req.params.person,
+        address: 'California',
+        email: 'blah@gmail.com',
+        number: req.params.number || 1
+      };
+
+      // Add them to the spreadsheet
+      sheet.addRow(person, function() {
+
+        // Onwards!
+        callback(null, sheet);
+      });
+    },
+
+    async.apply(getPeople),
+    async.apply(showPeopleTable),
+    function (msg, callback) {
+      // Output list of people
+      res.status(200).send(msg);
+    }
+  ]);
+});
+
+// Helper function that callbacks with the worksheet we want
+var getGoogleSheet = function(callback) {
+  // spreadsheet key is the long id in the sheets URL
+  var doc = new GoogleSpreadsheet(SETTINGS.DOC_ID);
+
+  // Authorize
+  doc.useServiceAccountAuth(creds, function (err) {
+
+    // Get Google Spreadsheet info
+    doc.getInfo(function (err, info) {
+      if (err) {
+        res.status(200).send(err);
+      }
+
+      console.log('Loaded: ' + info.title + ' by ' + info.author.email);
+
+      // Return specific worksheet
+      callback(null, info.worksheets[SETTINGS.SHEET]);
+    });
+  });
+};
+
+// Takes worksheet, callbacks with an array of people
+var getPeople = function(sheet, callback) {
+  var people = [];
+
+  // Grab only 20 rows right now
+  sheet.getRows({
+    offset: 1,
+    limit: 20,
+    orderby: 'col1'
+  }, function (err, rows) {
+
+    // No additional formatting right now
+    rows.forEach(function (row, i) {
+      people[i] = row;
+    });
+
+    callback(null, people);
+  });
+};
+
+// Takes array of people, builds janky html table
+var showPeopleTable = function(people, callback) {
+  console.log(people.length, 'people');
+
+  var msg = "<table>";
+
+  people.forEach(function (row, i) {
+    msg += '<tr><td>' + people[i].name + '</td><td>' + people[i].address + '</td></tr>';
+  });
+
+  msg += "</table>";
+
+  callback(null, msg);
+};
 
 // Start the server
 var server = app.listen(process.env.PORT || '8080', function () {
